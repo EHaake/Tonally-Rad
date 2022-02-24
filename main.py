62f788206b9c1fdab4e7f73dd66c20abe8010e22
Erik Haake <erik@erikhaakephoto.com>
2022-02-23 16:16:01 -0800

    Get selected points into list as x,y tuples

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import argparse
+from asyncio import selector_events
 import cv2
 import numpy as np
 
 
 is_drawing = False
 prev_x, prev_y = None, None
+selected_points = []
 
 
 def draw(event, x, y, flags, param):
-    global prev_x, prev_y, is_drawing
+    global prev_x, prev_y, is_drawing, selected_points
 
     if event == cv2.EVENT_LBUTTONDOWN:
         is_drawing = True
@@ -17,12 +19,16 @@ def draw(event, x, y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE:
         if is_drawing:
             cv2.line(img, (prev_x, prev_y), (x, y),
-                     color=(0, 0, 255), thickness=5)
+                     color=(0, 0, 255), thickness=10)
             prev_x, prev_y = x, y
+            # add points to list as tuples
+            selected_points.append((x, y))
 
     elif event == cv2.EVENT_LBUTTONUP:
         is_drawing = False
-        cv2.line(img, (prev_x, prev_y), (x, y), color=(0, 0, 255), thickness=5)
+        cv2.line(img, (prev_x, prev_y), (x, y),
+                 color=(0, 0, 255), thickness=10)
+        print(selected_points)
 
 
 if __name__ == '__main__':
