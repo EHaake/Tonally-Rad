b16d90d4e8ca67b24507abae4b82b2d496ee3cd7
Erik Haake <erik@erikhaakephoto.com>
2022-02-24 11:14:36 -0800

    Get the mean channel values of selected points

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import cv2
 import numpy as np
 
 
-is_drawing = False
-prev_x, prev_y = None, None
-selected_points = []
-
-
 def draw(e, x, y, flags, param):
     '''
     Draws a line from the previous point to the current point using the mouse
@@ -42,21 +37,21 @@ def constrain_pixels(points, intensity_threshold=20):
     :param points: list of (x, y) tuples
     :param intensity_threshold: threshold for the intensity value
     """
-
     # remove duplicate points
     points = np.unique(np.array(points), axis=0)
-    print(points)
 
     # split the lab image into its channels
     l_chan, a_chan, b_chan = cv2.split(lab_img)
 
-    # collect the lightness values of the selected pixels
+    # collect the channel values of the selected pixels
     x_pts = points[:, 0]
     y_pts = points[:, 1]
-    luminance_vals = l_chan[y_pts, x_pts]
+    l_vals, a_vals, b_vals = l_chan[y_pts,
+                                    x_pts], a_chan[y_pts, x_pts], b_chan[y_pts, x_pts]
 
-    # find the mean of the lightness values
-    luminance_mean = np.mean(luminance_vals)
+    # find the mean of the channel values
+    l_mean, a_mean, b_mean = np.mean(l_vals), np.mean(a_vals), np.mean(b_vals)
+    print(l_mean, a_mean, b_mean)
 
 
 if __name__ == '__main__':
@@ -68,8 +63,13 @@ if __name__ == '__main__':
 
     # load the image using opencv
     img = cv2.imread(image_path)
-    # convert image to LAB space and split into its channels
+    # convert image to LAB space
     lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    # declare our global variables
+    is_drawing = False
+    prev_x, prev_y = None, None
+    selected_points = []
 
     # create a window and bind the mouse callback function
     cv2.namedWindow('draw')
