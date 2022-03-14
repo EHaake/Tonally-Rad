831dfd34d94170aee94888c2c27ec690bfb06a31
Erik Haake <erik@erikhaakephoto.com>
2022-03-13 19:26:22 -0700

    Update with (non-working) tone mapping functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
 import cv2
 import numpy as np
-import pcg
-
+import scipy.sparse as sp
+import scipy.sparse.linalg as spl
 
 # Constants alpha, lambda, and epsilon
 ALPHA = 1
@@ -10,7 +10,7 @@ LAMBDA = 0.2
 EPSILON = 0.0001
 
 
-def draw(e, x, y, flags, param):
+def select_pixels(e, x, y, flags, param):
     '''
     Draws a line from the previous point to the current point using the mouse
     Indicate the areas of the image that should be selected
@@ -27,7 +27,7 @@ def draw(e, x, y, flags, param):
     elif e == cv2.EVENT_MOUSEMOVE:
         if is_drawing:
             cv2.line(img, (prev_x, prev_y), (x, y),
-                     color=(0, 255, 0), thickness=10)
+                     color=(0, 255, 0), thickness=8)
             prev_x, prev_y = x, y
             selected_points.append([x, y])
 
@@ -76,11 +76,12 @@ def constrain_pixels(points, intensity_threshold=20, chroma_threshold=10, gaussi
     if gaussian:
         l_weight = np.exp(-((l_chan - l_mean) ** 2) /
                           (intensity_threshold ** 2))
-        a_weight = np.exp(-((a_chan - a_mean) ** 2) /
-                          (chroma_threshold ** 2))
-        b_weight = np.exp(-((b_chan - b_mean) ** 2) /
-                          (chroma_threshold ** 2))
-        weights = np.array([l_weight, a_weight, b_weight])
+        # a_weight = np.exp(-((a_chan - a_mean) ** 2) /
+        #                   (chroma_threshold ** 2))
+        # b_weight = np.exp(-((b_chan - b_mean) ** 2) /
+        #                   (chroma_threshold ** 2))
+        # weights = np.array([l_weight, a_weight, b_weight])
+        weights = np.array([l_weight])
     else:
         weights = np.array([1., 1., 1.])
 
@@ -88,17 +89,15 @@ def constrain_pixels(points, intensity_threshold=20, chroma_threshold=10, gaussi
     # weights /= weights.size
     # weights /= 3
 
-    print(weights.shape)
-
     # create the output image showing the selected pixels
-    output_image = cv2.merge((l_chan, a_chan, b_chan))
-    output_image = cv2.cvtColor(output_image, cv2.COLOR_LAB2BGR)
+    overlay_image = cv2.merge((l_chan, a_chan, b_chan))
+    overlay_image = cv2.cvtColor(overlay_image, cv2.COLOR_LAB2BGR)
 
     # set the selected pixels to magenta
-    output_image[mask] = (255, 0, 255)
+    overlay_image[mask] = (255, 0, 255)
 
     # show the image
-    cv2.imshow('masked_img', output_image)
+    cv2.imshow('masked_img', overlay_image)
 
 
 def lischinski_tmo():
@@ -109,43 +108,66 @@ def lischinski_tmo():
     l_chan, a_chan, b_chan = cv2.split(lab_img)
 
     # extract the max and min of the l channel
-    max_l = np.max(l_chan)
-    min_l = np.min(l_chan)
+    min_l_log = np.log2(np.min(l_chan) + EPSILON)
+    if min_l_log < 0:
+        min_l_log = EPSILON
+    max_l_log = np.log2(np.max(l_chan))
+    print("min_l_log: {}, max_l_log: {}".format(min_l_log, max_l_log))
 
-    min_l_log = np.log2(min_l + EPSILON)
+    num_zones = int(np.ceil(max_l_log - min_l_log))
 
-    Z = np.ceil(np.log2(max_l) - min_l_log)
+    # choose the representative Rz (luminance value) for each zone z
+    f_stop_map = np.zeros(l_chan.shape)
+    # log_avg = np.log2(np.mean(l_chan))
+    log_avg = log_mean(l_chan)
+    print("log_avg: {}".format(log_avg))
 
-    # choose the representative Rz for each zone
-    f_stop_map = np.zeros(len(l_chan))
-    log_avg = np.log2(l_chan).mean()
-
-    for i in range(1, int(Z)):
+    for i in range(num_zones):
+        lower_l = np.power(2, i - 1 + min_l_log)
+        upper_l = np.power(2, min_l_log + i)
         indices = np.where(np.logical_and(
-            np.power(2, min_l_log + i) <= l_chan, np.power(2, min_l_log + i + 1) > l_chan))
-        if len(indices[0]) > 0:
+            l_chan >= lower_l, l_chan < upper_l))
+        # print("indices: {}".format(indices))
+
+        if len(indices) > 0:
             Rz = np.median(l_chan[indices])
             Rz = (ALPHA * Rz) / log_avg
             f = Rz / (1 + Rz)
+            # apply np.log2(f / Rz) to f_stop_map at indices
+            # print("indices: ", indices)
             f_stop_map[indices] = np.log2(f / Rz)
+            # if i is 1:
+            # print("f_stop_map shape: {}".format(f_stop_map.shape))
 
     weights = np.ones(l_chan.shape) * 0.07
-    exposure_factor = compute_exposure_fn(
-        np.log2(EPSILON + l_chan), f_stop_map, weights)
 
+    # fill f_stop_map with random values between 0 and 1
+    # f_stop_map = np.random.rand(l_chan.shape[0], l_chan.shape[1])
+
+    # compute the expsoure function
+    exposure_factor = compute_exposure_fn(
+        np.log(EPSILON + l_chan), f_stop_map, weights, log_avg)
+    # exposure_factor = compute_exposure_fn(
+    #     l_chan, f_stop_map, weights, log_avg)
+    # exposure_factor = np.ones(l_chan.shape)
+
+    # apply the exposure factor to the image
+    l_chan = np.uint8(l_chan * exposure_factor)
     output_image = cv2.merge((l_chan, a_chan, b_chan))
     output_image = cv2.cvtColor(output_image, cv2.COLOR_LAB2BGR)
 
-    # apply the exposure factor to the image
-    for i in range(4):
-        output_image[:, :, i] = output_image[:, :, i] * \
-            np.pow(2, exposure_factor)
+    # correct the gamma of the tone mapped image
+    # output_image = gamma_correction(output_image, 2.0)
 
     # show the image
-    cv2.imshow('output_image', output_image)
+    # cv2.imshow('output_image', output_image)
+    print("==========================")
+    print("DONE :)")
+    print("==========================")
+    return output_image
 
 
-def compute_exposure_fn(l_chan, f_stop_map, weights):
+def compute_exposure_fn(l_chan, f_stop_map, weights, log_avg):
     """
     Compute the exposure_fn by solving Lischinski minimization function
     Solve the linear system A f = b, where b = w * g
@@ -153,49 +175,104 @@ def compute_exposure_fn(l_chan, f_stop_map, weights):
     :param f_stop_map: the f_stop map
     :param weights: the weights of the pixels
     """
-    # compute the exposure factor
-    # exposure_factor = np.sum(np.multiply(np.power(2, f_stop_map), weights))
-
-    w, h = l_chan.shape
+    h, w = f_stop_map.shape
     n = w * h
 
     # generate the b vector
-    g = f_stop_map * weights
+    g = log_avg * weights
+    print("g max: {}, g min: {}".format(np.max(g), np.min(g)))
     # reshape g into a vector b
     b = np.reshape(g, (n, 1))
 
-    # compute the gradients
-    y_gradient = np.diff(l_chan, axis=0)
-    x_gradient = np.diff(l_chan, axis=1)
-
-    y_gradient = -LAMBDA / (np.power(np.abs(y_gradient), ALPHA) + EPSILON)
-    x_gradient = -LAMBDA / (np.power(np.abs(x_gradient), ALPHA) + EPSILON)
-
-    y_gradient = np.pad(y_gradient, ((0, 0), (1, 1)), 'constant')
-    x_gradient = np.pad(x_gradient, ((1, 1), (0, 0)), 'constant')
-
-    # reshape into a column vector
-    # y_gradient = np.reshape(y_gradient, (n, 1))
-    y_gradient = y_gradient[..., None]
-    x_gradient = x_gradient[..., None]
+    x_gradient, y_gradient = compute_gradients(l_chan)
 
     # generate the A matrix
-    A = np.diag(np.ones(n)) + np.multiply(y_gradient, x_gradient)
+    A = generate_A(x_gradient, y_gradient, h, w, f_stop_map)
+
+    # Solve the sparse linear system using Conjugate Gradients
+    f, info = spl.cg(A, b, tol=1e-6, maxiter=100)
+    print('info: {}'.format(info))
+    f = np.reshape(f, (h, w), order='F')
+    print("Max value in f: {}".format(np.max(f)))
+
+    return f
+
+
+def generate_A(x_gradient, y_gradient, h, w, f_stop_map):
+    """
+    Generate the A matrix
+    :param x_gradient: the x gradient
+    :param y_gradient: the y gradient
+    :param h: the height of the image
+    :param w: the width of the image
+    :return: the A matrix
+    """
+    n = h * w
+    A = sp.diags([x_gradient, y_gradient], offsets=[-h, -1],
+                 shape=(n, n), dtype=np.float32)
     A = A + A.T
 
-    g00 = np.pad(x_gradient, h, 'constant')
-    g00 = g00[1: -h]
+    g00 = np.pad(x_gradient, 0, 'constant')
+    # g00 = g00[0: -h]
 
-    g01 = np.pad(y_gradient, w, 'constant')
-    g01 = g01[1: -1]
+    g01 = np.pad(y_gradient, 0, 'constant')
+    # g01 = g01[0: -1]
+    # D = np.reshape(weights, (n, 1)) - (g00 + x_gradient + g01 + y_gradient)
+    D = np.reshape(f_stop_map, (n, 1)) - (g00 + x_gradient + g01 + y_gradient)
+    print("D shape: {}".format(D.shape))
+    # A = A + np.diag(D)
+    # A = A + sp.diags(D, 0, (n, n), dtype=np.float32)
+    A = A + D
 
-    D = np.reshape(weights, (n, 1)) - (g00 + x_gradient + g01 + y_gradient)
-    A = A = np.diag()
+    return A
 
-    result = np.linalg.solve(A, b)
-    exposure_factor = np.reshape(result, (w, h))
 
-    return exposure_factor
+def compute_gradients(l_chan):
+    """
+    Compute the gradients of the image
+    :param l_chan: the l channel of the image
+    :return: the gradients of the image
+    """
+    y_gradient = np.diff(l_chan, 1, axis=0)
+    x_gradient = np.diff(l_chan, 1, axis=1)
+
+    y_gradient = (-LAMBDA) / (np.power(np.abs(y_gradient), ALPHA) + EPSILON)
+    x_gradient = (-LAMBDA) / (np.power(np.abs(x_gradient), ALPHA) + EPSILON)
+
+    y_gradient = np.pad(y_gradient, ((0, 1), (0, 0)), 'constant')
+    x_gradient = np.pad(x_gradient, ((0, 0), (0, 1)), 'constant')
+
+    # reshape into a column vector
+    y_gradient = np.reshape(
+        y_gradient, y_gradient.shape[0] * y_gradient.shape[1], order='C')
+    x_gradient = np.reshape(
+        x_gradient, x_gradient.shape[0] * x_gradient.shape[1], order='C')
+
+    return x_gradient, y_gradient
+
+
+def gamma_correction(img, gamma=1.0):
+    """
+    Apply gamma correction to the image
+    :param img: the image
+    :param gamma: the gamma value
+    :return: the gamma corrected image
+    """
+    inverse_gamma = 1.0 / gamma
+    values = np.array([((i / 255.0) ** inverse_gamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(img, values)
+
+
+def log_mean(lum, delta=0.000001):
+    """
+    Compute the log mean of the luminance
+    :param lum: the luminance channel
+    :param delta: the delta
+    :return: the log mean
+    """
+    # return np.log(np.mean(np.exp(lum))) / np.log(1 + delta)
+    return np.exp(np.mean(np.log(lum + delta)))
 
 
 if __name__ == '__main__':
@@ -216,11 +293,13 @@ if __name__ == '__main__':
     selected_points = []
 
     # create a window and bind the mouse callback function
-    cv2.namedWindow('draw')
-    cv2.setMouseCallback('draw', draw)
+    # cv2.namedWindow('draw')
+    # cv2.setMouseCallback('draw', draw)
 
+    auto_img = lischinski_tmo()
     # keep the window open until the user presses escape key
     while True:
-        cv2.imshow('draw', img)
+        # cv2.imshow('draw', img)
+        cv2.imshow('auto', auto_img)
         if cv2.waitKey(1) & 0xFF == 27:
             break
